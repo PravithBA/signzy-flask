f644f210d0929696835337e20549196ec0424d4a
pravithba <pravithba10@gmail.com>
2022-07-22 14:45:09 +0530

    yeqa

--- app.py
+++ app.py
@@ -15,23 +15,6 @@ app.config[
 ] = f"postgresql://{os.environ['POSTGRES_USERNAME']}:{os.environ['POSTGRES_PASSWORD']}@0.0.0.0:5432/{os.environ['POSTGRES_DATABASE_NAME']}"
 db.init_app(app)
 
-
-class IdentityModel(db.Model):
-    __tablename__ = "identity_signzy"
-
-    identity_uuid = db.Column(db.BigInteger, unique=True, primary_key=True)
-    identity_access_token = db.Column(db.String(), unique=True, nullable=False)
-    identity_id = db.Column(db.String(), unique=True, nullable=False)
-
-    def __init__(self, identity_uuid, identity_access_token, identity_id):
-        self.identity_uuid = identity_uuid
-        self.identity_access_token = identity_access_token
-        self.identity_id = identity_id
-
-    def __repr__(self):
-        return f"<Identity {self.identity_id}>"
-
-
 with app.app_context():
     db.create_all()
 
